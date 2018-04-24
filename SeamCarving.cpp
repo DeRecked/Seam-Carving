3b121f22e80cd32365abf412b497ff1f30770be3
Derrick Patterson <derricklpatterson@gmail.com>
2018-04-24 12:26:39 -0400

    Reading in header informatin; added ImageProcessor cpp and hpp files

--- SeamCarving.cpp
+++ SeamCarving.cpp
@@ -2,19 +2,46 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char** argv) {
-	if (argc < 4)
-		throw "Usage: PROGRAM FILENAME NUM_VERTICAL_SEAMS NUM_HORIZONTAL_SEAMS";
-	else {
-		std::string filename = argv[1];
-		std::ifstream image(filename.c_str(), std::ios::binary);
+#include "ImageProcessor.hpp"
 
-		if (!image.is_open())
-			throw "Could not open image file";
+int main(int argc, char** argv) {
+	std::fstream image, image_processed;
+	std::string header, xy, max_gray;
+
+	try {
+		if (argc < 4)
+			throw "Usage: PROGRAM FILENAME NUM_VERTICAL_SEAMS NUM_HORIZONTAL_SEAMS";
+		else {
+
+			std::string filename = argv[1];
+			image.open(filename.c_str(), std::ios::in, std::ios::binary);
+
+			filename.erase(filename.length() - 4, 4);
+			std::string filename_processed = filename + "_processed.pgm";
+			image_processed.open(filename_processed.c_str(), std::ios::out, std::ios::binary);
+
+			if (!image.is_open())
+				throw "Could not open image file";
+
+			if (!image_processed.is_open())
+				throw "Could not open processed image output stream";
+		}
+	} catch (const char* err) {
+		std::cout << "The application threw an exception:" << err << std::endl;
 	}
 
 	int vertical_seams = atoi(argv[2]);
 	int horizontal_seams = atoi(argv[3]);
+
+	std::getline(image, header);
+	std::getline(image, xy);
+	
+	if (xy[0] == '#')
+		std::getline(image, xy);
+
+	std::getline(image, max_gray);
+
+	std::cout << header << "\n" << xy << "\n" << max_gray << std::endl;
 
 	return 0;
 }
